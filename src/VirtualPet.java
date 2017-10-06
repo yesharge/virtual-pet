8205b0e3d3579ad2d073909a03c7e28c0ab7ff7e
yesharge <yeshargeabrha@gmail.com>
2017-10-06 10:39:57 -0400

    First commit

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,9 +1,11 @@
 
 public class VirtualPet {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	private String hunger;
+	private String thirst;
+	private String waste;
+	private String boredom;
+	private String sickness;
+	
+	
 
 }
