a1385cf8457782fed559c59ad140e83a31e6361f
yesharge <yeshargeabrha@gmail.com>
2017-10-06 13:51:19 -0400

    Basic outline

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,11 +1,62 @@
 
 public class VirtualPet {
-	private String hunger;
-	private String thirst;
-	private String waste;
-	private String boredom;
-	private String sickness;
+	private int hunger;
+	private int thirst;
+	private int waste;
+	private int boredom;
+	private int sickness;
+	private int tiredness;
 	
 	
+	public VirtualPet() {
+	this.hunger =(int) Math.random()*100+1;
+	this.thirst =(int) Math.random()*100+1;
+	this.waste  =(int) Math.random()*100+1;
+	this.boredom =(int) Math.random()*100+1;
+	this.sickness =(int) Math.random()*100+1;
+	this.tiredness =(int) Math.random()*100+1;
+	}
+	public void feedMe () {
+		hunger-=50;
+		thirst-=30;
+		
+	}
+	public void playWithMe () {
+		boredom-=45;
+		sickness+=20;
+		tiredness+=35;
+		hunger+=30;
+	}
+	public void sleep () {
+		tiredness-=45; 
+	}
+	public void takeToDoctor () {
+		sickness-=75;
+	}
+	public void tick () {
+		hunger+=10;
+		thirst+=10;
+		boredom+=10;
+		sickness+=10;
+		tiredness+=10;
+		
+	}
+	public int hunger() {
+		return hunger;
+	}
+	public int thirst() {
+		
+		return thirst;
+	}
+	public int boredom() {
+		return boredom;
+	}
+	public int sickness() {
+	
+		return sickness;
+	}
+	public int tiredness() {
+		return tiredness;
+	}
 
 }
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -1,9 +1,53 @@
+import java.util.Scanner;
 
 public class VirtualPetApp {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		VirtualPet pet = new VirtualPet();
+
+		System.out.println("Hello, welcome to Virtual Pet" + "\nEnter a name for your zebra.");
+		String petName = input.nextLine();
+
+		System.out.println("Below are the needs and levels of " + petName + " the zebra. ");
+		System.out.println("Hunger: " + pet.hunger() + "\nThirst: " + pet.thirst() + "\nBoredom: " + pet.boredom()
+				+ "\nSickness: " + pet.sickness() + "\nTiredness: " + pet.tiredness());
+String userInput;
+		do {
+		System.out.println(
+				"What do you want to do? " + "\n1. Feed " + petName + "\n2. Play with " + petName + "\n3. Put " + petName
+						+ " to sleep" + "" + "\n4. Take " + petName + " to the doctors" +"\n5. Check "+petName+" levels "+"\n6. Exit the game");
+		 userInput = input.nextLine();
+		
+			if (userInput.equals("1")) {
+				pet.feedMe();
+				pet.tick();
+			}
+			else if(userInput.equals("2")) {
+				pet.playWithMe();
+				pet.tick();
+			}
+			else if (userInput.equals("3")) {
+				pet.sleep();
+				pet.tick();
+			}
+			else if (userInput.equals("4")) {
+				pet.takeToDoctor();
+				pet.tick();
+			}
+			else if (userInput.equals("5")) {
+				System.out.println("Hunger: " + pet.hunger() + "\nThirst: " + pet.thirst() + "\nBoredom: " + pet.boredom()
+				+ "\nSickness: " + pet.sickness() + "\nTiredness: " + pet.tiredness());
+		System.out.println();
+			}
+			 if (pet.hunger()>=90) {
+				System.out.println(petName+" is starving! Feed them asap!"); 
+			 }
+			 if (pet.tiredness()>75) {
+				 System.out.println(petName+ " is really tired. Nap time");
+			 }
+		
+	}while (!userInput.equals("6"));
 
 	}
-
 }
