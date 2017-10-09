78174141fde5ea97b7cdee5dd4b3e27174be531a
yesharge <yeshargeabrha@gmail.com>
2017-10-08 20:30:55 -0400

    Final commit

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,62 +1,105 @@
+import java.util.Random;
 
 public class VirtualPet {
+	//Instance Variables
 	private int hunger;
-	private int thirst;
-	private int waste;
 	private int boredom;
 	private int sickness;
 	private int tiredness;
-	
-	
+	private int selfCare;
+	Random random = new Random();
+//Constructor
 	public VirtualPet() {
-	this.hunger =(int) Math.random()*100+1;
-	this.thirst =(int) Math.random()*100+1;
-	this.waste  =(int) Math.random()*100+1;
-	this.boredom =(int) Math.random()*100+1;
-	this.sickness =(int) Math.random()*100+1;
-	this.tiredness =(int) Math.random()*100+1;
+
+		this.hunger = random.nextInt(100) + 1;
+		this.boredom = random.nextInt(100) + 1;
+		this.sickness = random.nextInt(100) + 1;
+		this.tiredness = random.nextInt(100) + 1;
 	}
-	public void feedMe () {
-		hunger-=50;
-		thirst-=30;
-		
+
+	public void feedMe() {
+		if (hunger <=50) {
+			hunger += 50;
+		}
 	}
-	public void playWithMe () {
-		boredom-=45;
-		sickness+=20;
-		tiredness+=35;
-		hunger+=30;
+
+	public void playWithMe() {
+		if (boredom <= 55) {
+			boredom += 45;
+			selfCare += 1;
+		}
+		if (sickness >= 10) {
+			sickness -= 10;
+			selfCare -= 1;
+		}
+		if (tiredness >= 35) {
+			tiredness -= 35;
+			selfCare -= 1;
+		}
+		if (hunger >= 30) {
+			hunger -= 30;
+			selfCare -= 1;
+		}
 	}
-	public void sleep () {
-		tiredness-=45; 
+
+	public void sleep() {
+		if (tiredness <=55) {
+		tiredness += 45;
+		selfCare += 1;
+		}
+		if (hunger>=30) {
+		hunger-=30;
+		}
 	}
-	public void takeToDoctor () {
-		sickness-=75;
+
+	public void takeToDoctor() {
+		if (sickness<=25) {
+		sickness += 75;
+		selfCare += 1;
+		}
 	}
-	public void tick () {
-		hunger+=10;
-		thirst+=10;
-		boredom+=10;
-		sickness+=10;
-		tiredness+=10;
-		
+
+	public void tick() {
+		selfCare = random.nextInt(10) + 1;
+		if (selfCare == 3) {
+			System.out.println("I'm sorry. Your pet has been ran over by a car.");
+			System.exit(0);
+		}
+		if (selfCare == 5) {
+			if (hunger>=10) {
+			hunger -= 10;
+			}
+			if (boredom>=10)  {
+			boredom += 10; 
+			}
+			if (sickness >= 10) {
+			sickness -= 10;
+			}
+			if (tiredness >10) {
+			tiredness += 10;
+		}
+		}
 	}
+
 	public int hunger() {
 		return hunger;
 	}
-	public int thirst() {
-		
-		return thirst;
-	}
+
 	public int boredom() {
 		return boredom;
 	}
+
 	public int sickness() {
-	
+
 		return sickness;
 	}
+
 	public int tiredness() {
 		return tiredness;
+	}
+
+	public int selfCare() {
+		return selfCare;
 	}
 
 }
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class VirtualPetApp {
-	
+
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		VirtualPet pet = new VirtualPet();
@@ -10,44 +10,50 @@ public class VirtualPetApp {
 		String petName = input.nextLine();
 
 		System.out.println("Below are the needs and levels of " + petName + " the zebra. ");
-		System.out.println("Hunger: " + pet.hunger() + "\nThirst: " + pet.thirst() + "\nBoredom: " + pet.boredom()
-				+ "\nSickness: " + pet.sickness() + "\nTiredness: " + pet.tiredness());
-String userInput;
+		System.out.println("Hunger: " + pet.hunger() + "\nBoredom: " + pet.boredom() + "\nSickness: " + pet.sickness()
+				+ "\nTiredness: " + pet.tiredness());
+		System.out.println();
+		String userInput;
 		do {
-		System.out.println(
-				"What do you want to do? " + "\n1. Feed " + petName + "\n2. Play with " + petName + "\n3. Put " + petName
-						+ " to sleep" + "" + "\n4. Take " + petName + " to the doctors" +"\n5. Check "+petName+" levels "+"\n6. Exit the game");
-		 userInput = input.nextLine();
-		
+			System.out.println("What do you want to do? " + "\n1. Feed " + petName + "\n2. Play with " + petName
+					+ "\n3. Put " + petName + " to sleep" + "" + "\n4. Take " + petName + " to the doctors"
+					+ "\n5. Check " + petName + " levels " + "\n6. Exit the game");
+			userInput = input.nextLine();
+			pet.tick();
 			if (userInput.equals("1")) {
 				pet.feedMe();
-				pet.tick();
-			}
-			else if(userInput.equals("2")) {
-				pet.playWithMe();
-				pet.tick();
-			}
-			else if (userInput.equals("3")) {
-				pet.sleep();
-				pet.tick();
-			}
-			else if (userInput.equals("4")) {
-				pet.takeToDoctor();
-				pet.tick();
-			}
-			else if (userInput.equals("5")) {
-				System.out.println("Hunger: " + pet.hunger() + "\nThirst: " + pet.thirst() + "\nBoredom: " + pet.boredom()
-				+ "\nSickness: " + pet.sickness() + "\nTiredness: " + pet.tiredness());
-		System.out.println();
-			}
-			 if (pet.hunger()>=90) {
-				System.out.println(petName+" is starving! Feed them asap!"); 
-			 }
-			 if (pet.tiredness()>75) {
-				 System.out.println(petName+ " is really tired. Nap time");
-			 }
-		
-	}while (!userInput.equals("6"));
+		}
 
+			if (userInput.equals("2")) {
+				pet.playWithMe();
+			}
+
+			if (userInput.equals("3")) {
+				pet.sleep();
+			} 
+			if (userInput.equals("4")) {
+				pet.takeToDoctor();
+			} 
+			if (userInput.equals("5")) {
+				System.out.println("Hunger: " + pet.hunger() + "\nBoredom: " + pet.boredom() + "\nSickness: "
+						+ pet.sickness() + "\nTiredness: " + pet.tiredness());
+				System.out.println();
+			}
+			if (pet.hunger() <=30) {
+				System.out.println(petName + " is getting hungry! Feed them asap!");
+			}
+			if (pet.tiredness() <=25) {
+				System.out.println(petName + " is really tired. Nap time");
+			}
+			if (pet.sickness() <= 5) {
+				System.out.println(petName + " is really ill. " + petName + " should see a doctor soon.");
+			}
+			if(pet.boredom() <= 30) {
+				System.out.println(petName+" is get lonely. Play time!");
+			}
+
+		} while (!userInput.equals("6"));
+		input.close();
+		System.exit(0);
 	}
 }
